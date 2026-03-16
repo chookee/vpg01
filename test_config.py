491329b8c2a2a8dbb7ef56cad0a7a74877d30cc2
chookee <chookee@inbox.ru>
2026-03-16 18:45:25 +0300

    Cleanup: Remove debug/test files from repo

--- test_config.py
+++
@@ -1,7 +0,0 @@
-from src.infrastructure.config import get_settings
-
-s = get_settings()
-print(f'LLM Provider: {s.llm_provider}')
-print(f'GenAPI Model: {s.genapi_model}')
-print(f'GenAPI Key: {s.genapi_key}')
-print(f'GenAPI configured: {s.is_genapi_configured()}')
